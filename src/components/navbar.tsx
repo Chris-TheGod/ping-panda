abe705a363a65b9b2007688c5b7401ce0d665897
Kristjan Lääne <xonproject@gmail.com>
2024-12-08 13:41:24 +0200

    navbar + auth finish

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,8 @@
 import Link from "next/link"
 import MaxWidthWrapper from "./max-with-wrapper"
 import { SignOutButton } from "@clerk/nextjs"
-import { Button } from "./ui/button"
+import { Button, buttonVariants } from "./ui/button"
+import { ArrowRight } from "lucide-react"
 
 export default function Navbar() {
   const user = false
@@ -18,10 +19,55 @@ export default function Navbar() {
             {user ? (
               <>
                 <SignOutButton>
-                  <Button>Sign out</Button>
+                  <Button size="sm" variant="ghost">
+                    Sign out
+                  </Button>
                 </SignOutButton>
+
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "flex items-center gap-1",
+                  })}
+                >
+                  Dashboard <ArrowRight className="ml-1.5 size-4" />
+                </Link>
               </>
-            ) : null}
+            ) : (
+              <>
+                <Link
+                  href="/dashboard"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Pricing
+                </Link>
+                <Link
+                  href="/sign-in"
+                  className={buttonVariants({
+                    size: "sm",
+                    variant: "ghost",
+                  })}
+                >
+                  Sign in
+                </Link>
+
+                <div className="h-8 w-px bg-gray-200" />
+
+                <Link
+                  href="/sign-up"
+                  className={buttonVariants({
+                    size: "sm",
+                    className: "flex items-center gap-1.5",
+                  })}
+                >
+                  Sign up <ArrowRight className="size-4" />
+                </Link>
+              </>
+            )}
           </div>
         </div>
       </MaxWidthWrapper>
