67a7a783b9b792f3703755eeea8a8fde93dd1b14
Kristjan Lääne <xonproject@gmail.com>
2024-12-08 11:54:56 +0200

    custom heading components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,11 @@ export default function Home() {
   return (
     <>
       <section className="relative py-24 sm:py-32 bg-brand-25">
-        <MaxWidthWrapper></MaxWidthWrapper>
+        <MaxWidthWrapper className="text-center">
+          <div className="relative mx-auto text-center flex flex-col items-center gap-10">
+            <div></div>
+          </div>
+        </MaxWidthWrapper>
       </section>
       <section></section>
       <section></section>
