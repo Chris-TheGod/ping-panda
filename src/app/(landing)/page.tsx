2bc1ba34f658282406e703c76a41606d95e38875
Kristjan Lääne <xonproject@gmail.com>
2024-12-09 14:43:08 +0200

    bento grid 4th element startÄ

--- src/app/(landing)/page.tsx
+++ src/app/(landing)/page.tsx
@@ -6,6 +6,8 @@ import MockDiscordUI from "@/components/mock-discord-ui"
 import { AnimatedList } from "@/components/AnimatedList"
 import DiscordMessage from "@/components/discord-message"
 import Image from "next/image"
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
+import { oneDark } from "react-syntax-highlighter/dist/esm/styles/prism"
 
 export default function Home() {
   return (
@@ -203,6 +205,39 @@ export default function Home() {
               </div>
 
               <div className="pointer-events-none absolute inset-px rounded-lg shadow ring-1 ring-black/5" />
+            </div>
+
+            {/* fourth bento grid element */}
+            <div className="relative row-span-2">
+              <div className="absolute inset-px rounded-lg bg-white max-lg:rounded-b-[2rem] lg:rounded-r-[2rem]" />
+
+              <div className="relative flex h-full flex-col overflow-hidden rounded-[calc(theme(borderRadius.lg)+1px)] max-lg:rounded-b-[calc(2rem+1px)] lg:rounded-r-[calc(2rem+1px)]">
+                <div className="px-8 pb-3 pt-8 sm:px-10 sm:pb-0 sm:pt-10">
+                  <p className="mt-2 text-lg/7 font-medium tracking-tight text-brand-950 max-lg:text-center">
+                    Easy Integration
+                  </p>
+                  <p className="mt-2 max-w-l text-sm/6 text-gray-600 max-lg:text-center">
+                    Connect PingBot with your existing workflows in minutes and
+                    call our intuitive logging API from any language.
+                  </p>
+                </div>
+
+                <div className="relative min-h-[30rem] w-full grow">
+                  <div className="absolute bottom-0 left-10 right-0 top-10 overflow-hidden rounded-tl-xl bg-gray-900 shadow-2xl">
+                    <div className="flex bg-gray-800/40 ring-1 ring-white/5">
+                      <div className="-mb-px flex text-sm/6 font-medium text-gray-400">
+                        <div className="border-b border-r border-b-white/20 border-r-white/10 bg-white/5 px-4 py-2 text-white">
+                          pingbot.js
+                        </div>
+                      </div>
+                    </div>
+
+                    <div className="overflow-hidden">
+                      <div className="max-h-[30rem]"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </MaxWidthWrapper>
