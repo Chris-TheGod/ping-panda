bf2d6bc66f8ffb252ddb199f97e53aaa0b45047d
Kristjan Lääne <xonproject@gmail.com>
2024-12-10 15:31:08 +0200

    add user to db

--- src/app/(auth)/welcome/page.tsx
+++ src/app/(auth)/welcome/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 // sync auth status do db
 
 import Heading from "@/components/heading"
@@ -5,15 +6,25 @@ import LoadingSpinner from "@/components/loading-spinner"
 import { client } from "@/lib/client"
 import { useQuery } from "@tanstack/react-query"
 import { LucideProps } from "lucide-react"
+import { useRouter } from "next/navigation"
+import { useEffect } from "react"
 
 export default function Page() {
+  const router = useRouter()
   const { data } = useQuery({
     queryFn: async () => {
       const res = await client.auth.getDatabaseSyncStatus.$get()
       return await res.json()
     },
     queryKey: ["get-database-sync-status"],
+    refetchInterval: (query) => {
+      return query.state.data?.isSynced ? false : 1000
+    },
   })
+
+  useEffect(() => {
+    if (data?.isSynced) router.push("/dashboard")
+  }, [data, router])
 
   return (
     <div className="flex w-full flex-1 items-center justify-center px-4">
